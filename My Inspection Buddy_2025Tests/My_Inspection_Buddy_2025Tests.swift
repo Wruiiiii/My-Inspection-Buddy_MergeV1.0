f86237f1e1c65a9b489c9befcca38f48672d3971
Wruiiiii <ruiwang025@gmail.com>
2025-07-09 20:41:21 -0400

    Add files via upload

---
+++ My Inspection Buddy_2025Tests/My_Inspection_Buddy_2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Inspection_Buddy_2025Tests.swift
+//  My Inspection Buddy_2025Tests
+//
+//  Created by Rae Wang on 6/23/25.
+//
+
+import Testing
+@testable import My_Inspection_Buddy_2025
+
+struct My_Inspection_Buddy_2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
